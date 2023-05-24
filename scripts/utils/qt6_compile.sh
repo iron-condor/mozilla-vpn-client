ba8c2df615e3b844d96c9586f242f44ee51c17c5
Naomi Kirby <oskirby@gmail.com>
2023-05-24 13:40:39 -0700

    Try harder to disable qtwebengine

--- scripts/utils/qt6_compile.sh
+++ scripts/utils/qt6_compile.sh
@@ -141,6 +141,7 @@ bash ./configure \
   -skip qtquickeffectmaker \
   -skip qtquicktimeline \
   -skip qtwebengine  \
+  -skip qtwebview \
   -skip qtlocation \
   -skip qtmultimedia  \
   -skip qtserialport  \
